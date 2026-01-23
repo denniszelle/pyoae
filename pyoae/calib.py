8361402fd713a571b783d43cb15d67a3aa7396a3
Simon Tien <simon.tien@earlab.de>
2026-01-23 13:48:13 +0100

    fix: reallocate get_interp_transfer_function from speaker to mic calib

--- pyoae/calib.py
+++ pyoae/calib.py
@@ -186,18 +186,6 @@ class OutputCalibration:
         s = self.get_sensitivity(ch, f)
         return p / s
 
-    def get_interp_transfer_function(
-        self,
-        frequencies
-    ) -> npt.NDArray[np.float32]:
-        """Return interpolated transfer function"""
-        interpolated_tf = np.interp(
-            frequencies,
-            self.frequencies,
-            self.amplitudes
-        )
-        return interpolated_tf
-
 
 class MicroTransferFunction:
     """Interpolated transfer function of the microphone."""
@@ -266,3 +254,15 @@ class MicroTransferFunction:
         # in order to calculate the frequency-bin index)
         idx = np.argmin(np.abs(self.frequencies - f))
         return self.amplitudes[idx]
+
+    def get_interp_transfer_function(
+        self,
+        frequencies
+    ) -> npt.NDArray[np.float32]:
+        """Return interpolated transfer function"""
+        interpolated_tf = np.interp(
+            frequencies,
+            self.frequencies,
+            self.amplitudes
+        )
+        return interpolated_tf

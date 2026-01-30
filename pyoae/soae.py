39ba2c99df6478f1a6ef3735fc4e33d0ff1e2f93
Simon Tien <simon.tien@earlab.de>
2026-01-30 16:27:54 +0100

    fix: update soae module according to DSP merge

--- pyoae/soae.py
+++ pyoae/soae.py
@@ -25,19 +25,17 @@ from dataclasses import dataclass
 from datetime import datetime
 from logging import Logger
 import os
-from typing import Literal
 
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.lines import Line2D
 import numpy as np
 import numpy.typing as npt
-import scipy.signal
 
 from pyoae import get_logger
 from pyoae import helpers
 from pyoae.calib import MicroTransferFunction
-from pyoae.dsp.opt_avg import OptAverage
+from pyoae.dsp import averaging
 from pyoae.device.device_config import DeviceConfig
 from pyoae.msrmt_context import MsrmtContext
 from pyoae.protocols import MsrmtParams
@@ -49,6 +47,10 @@ from pyoae.sync import (
     SyncMsrmt,
     MsrmtState
 )
+
+
+SPECTRAL_PLOT_PADDING: float = 15.0
+"""Padding for y-limits of spectral plot in dB."""
 
 
 logger = get_logger()
@@ -108,220 +110,6 @@ def setup_plot(
         ax_spec.set_ylabel("Level (dBFS)")
 
     return ax_time, line_time, ax_spec, line_spec
-
-
-def average_spectrum(
-    x: npt.NDArray[np.float32],
-    fs: float,
-    window: str = 'hann',
-    window_samples: int = 256,
-    overlap_samples: int | None = None,
-    detrend: Literal['linear', 'constant'] = 'constant'
-) -> tuple[npt.NDArray[np.float32], npt.NDArray[np.float32]]:
-    """Estimates a welch spectrum with optimized averaging.
-
-    Args:
-        x: Float array as time-domain signal
-        fs: Sampling frequency of the measurement
-        window: Type of the window (see scipy.signal.get_window)
-        window_samples: Samples for each window
-        overlap_samples: Window overlap samples, window_samples/2 by default
-        detrend: Remove linear or constant trend beforehand
-
-    Returns:
-        tuple[frequencies, spec_avg]
-
-        - **frequencies**: Float array containing the corresponding frequencies
-            for the magnitudes.
-        - **spec_avg**: Float array with averaged spectrum scaled
-            according to the scaling input
-
-    """
-    if overlap_samples is None:
-        overlap_samples = window_samples // 2
-    step = window_samples - overlap_samples
-    x_seg = np.lib.stride_tricks.sliding_window_view(
-        x, window_shape=window_samples
-    )[::step]  # (num_blocks, num_samples)
-
-    if detrend:
-        x_seg = scipy.signal.detrend(x_seg, type=detrend, axis=-1)
-
-    win = scipy.signal.get_window(window, window_samples)
-    x_seg = x_seg * win
-    x_fft = np.fft.rfft(x_seg, n=window_samples, axis=-1)
-
-    # Magnitude with coherent-gain correction
-    spectrum = np.abs(x_fft)
-
-    # One-sided amplitude scaling: double only non-DC/non-Nyquist bins
-    if window_samples % 2 == 0:
-        spectrum[..., 1:-1] *= 2.0
-    else:
-        spectrum[..., 1:] *= 2.0
-
-    spectrum /= np.sum(win)  # coherent gain
-    frequencies = np.fft.rfftfreq(window_samples, 1/fs)
-
-    # Obtain RMS values of spectrum
-    rms_vals = np.sqrt(np.mean(np.square(spectrum), axis=1))
-
-    # Apply optimized averaging
-    opt_averager = OptAverage()
-    opt_averager.setup(len(rms_vals))
-    for i, rms_val_i in enumerate(rms_vals):
-        opt_averager.set_noise_value(i, rms_val_i)
-    opt_averager.evaluate_averaging()
-
-    accepted_idc = opt_averager.accepted_idx
-
-    if len(accepted_idc):
-        spec_avg = spectrum[accepted_idc].mean(axis=0)
-    else:
-        spec_avg = np.zeros(len(spectrum[0]))
-
-    return frequencies.astype(np.float32), spec_avg.astype(np.float32)
-
-
-def process_spectrum(
-    recorded_signal: npt.NDArray[np.float32],
-    fs: float,
-    window_samples: int,
-    correction_tf: MicroTransferFunction | None
-) -> npt.NDArray[np.float32]:
-    """Processes recorded signal and obtains spectrum from averaged data.
-
-    Args:
-        recorded_signal: Float array of measurement data
-        fs: Sampling frequency the signal was recorded with
-        window_samples: Number of samples for each window for
-          asynchronous spectrum estimations
-        correction_tf: Transfer function of the microphone
-
-    Returns:
-        Float array containing the asynchronous averaged spectrum
-    """
-
-    spectrum = None
-
-    if len(recorded_signal) > window_samples:
-        frequencies, spectrum = average_spectrum(
-            recorded_signal,
-            fs,
-            'hann',
-            window_samples,
-        )
-        if np.max(spectrum) == 0:
-            # pylint: disable=no-member
-            spectrum[:] = np.finfo(np.float32).eps
-            # pylint: enable=no-member
-        if correction_tf is None:
-            spectrum = 20 * np.log10(spectrum)
-        else:
-            corr_tf = correction_tf.get_interp_transfer_function(
-                frequencies
-            )
-            spectrum /= corr_tf
-            spectrum = 20 * np.log10(spectrum/20)
-
-    else:
-        spectrum = np.abs(np.fft.rfft(np.zeros(window_samples, np.float32)))
-    return spectrum
-
-
-def get_results(
-    sync_msrmt: SyncMsrmt,
-    msrmt_ctx: MsrmtContext,
-    input_ch: int,
-    index: int,
-) -> tuple[npt.NDArray[np.float32], npt.NDArray[np.float32]]:
-    """Processes data and returns plot results.
-
-    If the measurement is currently running, the recorded signal is obtained
-    and a asynchronously averaged spectrum is estimated.
-
-    Args:
-        sync_msrmt: Measurement object that handles the synchronized
-          measurement.
-        msrmt_ctx: Parameters and instances to control the measurement.
-
-    Returns:
-        tuple[recorded_signal, spectrum]
-
-        - **recorded_signal**: Float array with recorded signal
-        - **spectrum**: Float array with estimated spectrum
-
-    """
-
-    if sync_msrmt.state in [
-        MsrmtState.RECORDING,
-        MsrmtState.END_RECORDING,
-        MsrmtState.FINISHING,
-        MsrmtState.FINISHED
-    ]:
-
-        spectrum = None
-        recorded_signal = sync_msrmt.get_recorded_signal(input_ch)
-
-        if msrmt_ctx.input_trans_fun is None:
-            mic_trans_fun = None
-        else:
-            mic_trans_fun = msrmt_ctx.input_trans_fun[index]
-
-        spectrum = process_spectrum(
-            recorded_signal,
-            msrmt_ctx.fs,
-            msrmt_ctx.block_size,
-            mic_trans_fun
-        )
-
-        return recorded_signal, spectrum
-
-    return np.zeros(0,np.float32), np.zeros(0,np.float32)
-
-
-def plot_offline(
-    sync_msrmt: SyncMsrmt,
-    msrmt_ctx: MsrmtContext,
-    index: int
-) -> None:
-    """Plots the final results in a non-updating plot.
-
-    This function obtains the results from the measurement object, creates a
-    plot and shows the complete measurement as well as the spectral estimate.
-
-    Args:
-        sync_msrmt: Measurement object that handles the synchronized
-          measurement.
-        msrmt_ctx: Parameters and instances to control the measurement.
-
-    """
-    if sync_msrmt.state != MsrmtState.FINISHED:
-        return
-    recorded_signal, spectrum = get_results(
-        sync_msrmt,
-        msrmt_ctx,
-        sync_msrmt.hardware_data.get_unique_input_channels()[index],
-        index
-    )
-    ax_time, line_time, ax_spec, line_spec = setup_plot(
-        sync_msrmt.recording_data.msrmt_duration,
-        sync_msrmt.recording_data.fs,
-        msrmt_ctx.block_size,
-        msrmt_ctx.input_trans_fun is not None
-    )
-    line_time.set_xdata(np.arange(len(recorded_signal))/msrmt_ctx.fs)
-    line_time.set_ydata(recorded_signal)
-    ax_time.set_xlim(0, sync_msrmt.recording_data.msrmt_duration)
-    ax_time.set_xlabel("Recording Time (s)")
-
-    spec_min = min(spectrum[1:])
-    spec_max = max(spectrum)
-    padding = 15  # dB of padding on top and bottom
-    ax_spec.set_ylim(spec_min - padding, spec_max + padding)
-    line_spec.set_ydata(spectrum)
-    plt.tight_layout()
-    plt.show()
 
 
 @dataclass
@@ -418,13 +206,6 @@ class SoaeRecorder:
             get_input_channels(active_out_channels)
         )
 
-        # for i in range(n_in_channels):
-        #     self.msrmt_info.append(
-        #         SoaeMsrmtInfo(
-        #             msrmt_ctx,
-        #         )
-        #     )
-
         self.signals = [
             ZeroSignal() for _ in range(hw_data.get_stream_output_channels())
         ]
@@ -433,8 +214,7 @@ class SoaeRecorder:
             rec_data,
             hw_data,
             self.signals,
-            msrmt_params['block_duration'],
-            active_out_channels
+            msrmt_params['block_duration']
         )
 
     def record(self) -> None:
@@ -446,15 +226,13 @@ class SoaeRecorder:
         if self.msrmt_ctx.non_interactive:
             return
 
-
-
         for i, _ in enumerate(
             self.msrmt.hardware_data.get_unique_input_channels()
         ):
             self.logger.info(
                 'Showing offline results. Please close window to continue.'
             )
-            plot_offline(self.msrmt, self.msrmt_ctx, i)
+            self.plot_offline(i)
 
     def save_recording(self) -> None:
         """Stores the measurement data in binary file."""
@@ -482,12 +260,7 @@ class SoaeRecorder:
             ]
             file_name = "_".join(filter(None, parts))
             file_save_path = os.path.join(save_path, file_name)
-            recorded_signal, spectrum = get_results(
-                self.msrmt,
-                self.msrmt_ctx,
-                input_channel_i,
-                i,
-            )
+            recorded_signal, spectrum = self.get_results(i)
             np.savez(
                 file_save_path,
                 spectrum=spectrum,
@@ -495,3 +268,140 @@ class SoaeRecorder:
                 samplerate=DeviceConfig.sample_rate
             )
             self.logger.info("Measurement saved to %s.", file_save_path)
+
+    def _plot_offline(
+        self,
+        recorded_signal: npt.NDArray[np.float32],
+        spectrum: npt.NDArray[np.float32]
+    ) -> None:
+        """Helper to plot the final results in a non-updating plot.
+
+        This method obtains the results from the measurement object,
+        creates a plot and shows the complete measurement
+        as well as the RMS spectrum.
+
+        Args:
+            recorded_signal: recorded time signal
+            spectrum: RMS-averaged spectrum of recorded signal
+        """
+        ax_time, line_time, ax_spec, line_spec = setup_plot(
+            self.msrmt.recording_data.msrmt_duration,
+            self.msrmt.recording_data.fs,
+            self.msrmt_ctx.block_size,
+            self.msrmt_ctx.input_trans_fun is not None
+        )
+        line_time.set_xdata(np.arange(len(recorded_signal))/self.msrmt_ctx.fs)
+        line_time.set_ydata(recorded_signal)
+        ax_time.set_xlim(0, self.msrmt.recording_data.msrmt_duration)
+        ax_time.set_xlabel("Recording Time (s)")
+
+        spec_min = np.floor((min(spectrum[1:]) / 5)) * 5
+        spec_max = np.ceil(max(spectrum) / 5) * 5
+        # set y limits with padding
+        ax_spec.set_ylim(
+            spec_min - SPECTRAL_PLOT_PADDING,
+            spec_max + SPECTRAL_PLOT_PADDING
+        )
+        line_spec.set_ydata(spectrum)
+
+    def plot_offline(self, index: int = 0) -> None:
+        """Shows the final results in a polished plot."""
+        if self.msrmt.state != MsrmtState.FINISHED:
+            return
+        recorded_signal, spectrum = self.get_results(index)
+        self._plot_offline(recorded_signal, spectrum)
+        plt.tight_layout()
+        plt.show()
+
+
+    def get_results(
+        self,
+        index: int = 0,
+    ) -> tuple[npt.NDArray[np.float32], npt.NDArray[np.float32]]:
+        """Processes data and returns plot results.
+
+        If the measurement is currently running, the recorded signal is
+        obtained and a asynchronously averaged spectrum is estimated.
+
+        Args:
+            sync_msrmt: Measurement object that handles the synchronized
+            measurement.
+            msrmt_ctx: Parameters and instances to control the measurement.
+
+        Returns:
+            tuple[recorded_signal, spectrum]
+
+            - **recorded_signal**: Float array with recorded signal
+            - **spectrum**: Float array with estimated spectrum
+
+        """
+
+        if self.msrmt.state in [
+            MsrmtState.RECORDING,
+            MsrmtState.END_RECORDING,
+            MsrmtState.FINISHING,
+            MsrmtState.FINISHED
+        ]:
+
+            spectrum = None
+            recorded_signal = self.msrmt.get_recorded_signal(
+                self.msrmt.hardware_data.get_unique_input_channels()[index]
+            )
+
+            if self.msrmt_ctx.input_trans_fun is None:
+                mic_trans_fun = None
+            else:
+                mic_trans_fun = self.msrmt_ctx.input_trans_fun[index]
+
+            spectrum = self.process_spectrum(
+                recorded_signal,
+                mic_trans_fun
+            )
+
+            return recorded_signal, spectrum
+
+        return np.zeros(0,np.float32), np.zeros(0,np.float32)
+
+
+    def process_spectrum(
+        self,
+        recorded_signal: npt.NDArray[np.float32],
+        correction_tf: MicroTransferFunction | None = None,
+        window: str = 'hann'
+    ) -> npt.NDArray[np.float32]:
+        """Processes recorded signal and obtains spectrum from averaged data.
+
+        Args:
+            recorded_signal: Float array of measurement data
+
+        Returns:
+            Float array containing the asynchronous averaged spectrum
+        """
+
+        spectrum = None
+
+        if len(recorded_signal) > self.msrmt_ctx.block_size:
+            frequencies, spectrum = averaging.welch_spectrum(
+                recorded_signal,
+                self.msrmt_ctx.fs,
+                window,
+                self.msrmt_ctx.block_size,
+            )
+            if np.max(spectrum) == 0:
+                # pylint: disable=no-member
+                spectrum[:] = np.finfo(np.float32).eps
+                # pylint: enable=no-member
+            if correction_tf is None:
+                spectrum = 20 * np.log10(spectrum)
+            else:
+                spectrum /= correction_tf.get_interp_transfer_function(
+                    frequencies
+                )
+                spectrum = 20 * np.log10(spectrum/20)
+
+        else:
+            spectrum = np.abs(np.fft.rfft(np.zeros(
+                self.msrmt_ctx.block_size, np.float32
+            )))
+
+        return spectrum

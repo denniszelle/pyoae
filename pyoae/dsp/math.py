2ddc30cbb80f5719ab352d1db963a51dc00f6dcf
Dennis Zelle <dennis.zelle@earlab.de>
2026-03-03 11:29:44 +0100

    feat: add RMS calculation for 2D matrix

--- pyoae/dsp/math.py
+++ pyoae/dsp/math.py
@@ -7,3 +7,11 @@ import numpy.typing as npt
 def rms(y: npt.NDArray[np.float32 | np.float64]) -> float:
     """Calculates the RMS for a given signal."""
     return np.sqrt(np.mean(np.square(y)))
+
+
+def rms_2d(
+    y: npt.NDArray[np.float32 | np.float64],
+    axis: int = 0
+) -> npt.NDArray[np.float64]:
+    """Calculates the RMS along a axis for a 2D array."""
+    return np.sqrt(np.mean(np.square(y.astype(np.float64)), axis=axis))
